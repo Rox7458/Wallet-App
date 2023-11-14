4d0023288c3f5b02a034269fde985d1426730544
Rox745 <jagaaoguz.7000@gmail.com>
2023-11-14 23:53:12 +0300

    v.1.4

--- event.js
+++ event.js
@@ -1,12 +1,5 @@
 const date = document.querySelector(".date-number").innerText
-// const amount = document.querySelector(".amount-spent")
+const amount = document.querySelector(".amount-spent")
 const kindSpending = document.querySelector(".kind-spending").innerText
-
 const gider = document.querySelector(".outgoing")
-
 const saveBtn = document.querySelector(".btn-save")
-
-saveBtn.addEventListener("click", () => {
-    gider.innerText = document.querySelector(".amount-spent").innerText
-})
-
