ed2bc5976755eec5e4be5b769626835dcd2fa551
Rox745 <jagaaoguz.7000@gmail.com>
2023-11-22 18:54:17 +0300

    v-2.2

--- event.js
+++ event.js
@@ -1,14 +1,39 @@
 //buttons
 
 const addBtn = document.getElementById("btn-add")
-const addInput = document.getElementById("add-input")
-const addForm = document.getElementById("add-form")
+const btnSave = document.getElementById("btn-save")
 
 const formAdd = document.getElementById("form-add")
+const addInput = document.getElementById("add-input")
+
+// veriables
 
 let earnings = 0
 
-formAdd.addEventListener("submit" )
+//earning add form
+
+formAdd.addEventListener("submit", (e) => {
+    e.preventDefault()
+    earnings = earnings + Number(addInput.value)
+    localStorage.setItem("earnings", earnings)
+    formAdd.reset()
+})
+
+//window starter
+
+window.addEventListener("load", () => {
+    earnings = Number(localStorage.getItem("earnings")) || 0
+})
+
+// Expenditure Form
+
+btnSave.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    
+
+})
+
 
 
 
